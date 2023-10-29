68dad1dbfb892908c2b6785b1ba65b31468f3d17
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-10-29 09:28:40 +0100

    Inglese e alcolico/analcolico

--- src/app/elenco-lettera/elenco-lettera.component.ts
+++ src/app/elenco-lettera/elenco-lettera.component.ts
@@ -11,6 +11,16 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ElencoLettera implements OnInit {
     drinks: Drink[] = [];
+    lettera: string = "";
+    drinksAlchol: Drink[] = [];
+    drinksAnalchol: Drink[] = [];
+    selectFilter = {
+        tutti: "tutti",
+        alcolici: "alcolici",
+        analcolici: "analcolici",
+    };
+    selectedFilter: string = this.selectFilter.tutti;
+    esisteLettera: boolean = true;
 
     constructor(
         private http: HttpClient,
@@ -18,16 +28,41 @@ export class ElencoLettera implements OnInit {
         private activatedRoute: ActivatedRoute
     ) {}
 
-    lettera: string = "";
-
     ngOnInit() {
         this.activatedRoute.params.subscribe((params) => {
             this.lettera = params["lettera"];
             this.drinkService
                 .getElencoDrinks(this.lettera)
-                .subscribe((dati) => {
-                    if (dati.drinks !== null) this.drinks = dati.drinks;
+                .subscribe((response) => {
+                    if (response && response.drinks !== null) {
+                        this.drinks = response.drinks;
+                        for (let i = 0; i < this.drinks.length; i++) {
+                            if (this.drinks[i].strAlcoholic === "Alcoholic") {
+                                this.drinksAlchol.push(response.drinks[i]);
+                            } else if (
+                                this.drinks[i].strAlcoholic === "Non alcoholic"
+                            ) {
+                                this.drinksAnalchol.push(response.drinks[i]);
+                            }
+                        }
+                    } else {
+                        this.esisteLettera = false;
+                    }
                 });
         });
+    }
+
+    selectFilterFunction(scelta: string) {
+        switch (scelta) {
+            case this.selectFilter.tutti:
+                this.selectedFilter = this.selectFilter.tutti;
+                break;
+            case this.selectFilter.alcolici:
+                this.selectedFilter = this.selectFilter.alcolici;
+                break;
+            case this.selectFilter.analcolici:
+                this.selectedFilter = this.selectFilter.analcolici;
+                break;
+        }
     }
 }
